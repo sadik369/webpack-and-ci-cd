a2d946ddb859d6bc03147e4704ca3588be14832b
mohammed.sadik <mdsadik4749@gmail.com>
2026-02-01 00:34:13 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Hello, Webpack 5 + React!</h1>
+            <p>This app was built from scratch.</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js", // The entry point of your app
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true, // Cleans the dist folder before every build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Look for .js or .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/, // Look for .css files
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // Use this HTML as a template
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000, // Run on port 3000
+    open: true, // Open browser automatically
+    hot: true,
+  },
+  mode: "development",
+};
